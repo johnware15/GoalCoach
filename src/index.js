9f25cba56af35899a3d777403ccfb9a5f4b627ed
johnware15 <contact@johndware.com>
2018-09-23 16:03:32 -0700

    Set up ReactRouter and Firebase.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { firebaseApp } from './firebase';
+
+import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  if(user) {
+    console.log('User has signed in or up', user);
+  } else {
+    console.log('User has signed out or still needs to sign in.');
+  }
+})
 
 ReactDOM.render(
-  <div>App</div>, document.getElementById('root')
+  <BrowserRouter><div>
+    <Route path='/app' component={App} />
+    <Route path='/signin' component={SignIn} />
+    <Route path='/signup' component={SignUp} />
+    </div>
+  </BrowserRouter>, document.getElementById('root')
 )
