40ee31899f405c6952f8e3c3457cc2e009eb6483
johnware15 <contact@johndware.com>
2018-09-25 10:37:42 -0700

    Apply auth links to toggle between sign up and sign in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { firebaseApp } from '../firebase';
 
 class SignIn extends Component {
@@ -46,6 +47,7 @@ class SignIn extends Component {
           </button>
         </div>
         <div>{this.state.error.message}</div>
+        <div><Link to={'/signup'}>Sign up instead</Link></div>
       </div>
     )
   }
